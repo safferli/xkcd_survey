1225ab38fdd93e843c0b8f7cd5d74c053bff8c9c
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-09-03 18:57:59 +0200

    load all 4 google trends csv data

--- xkcd_words_impact.R
+++ xkcd_words_impact.R
@@ -6,8 +6,12 @@ library(dplyr)
 # library(tidyr)
 library(ggplot2)
 
+# Define your workspace: "X:/xxx/"
+wd <- "D:/github/xkcd_survey/"
+setwd(wd)
+
+
 terms <- c(
-  "NHL", 
   "Unitory", 
   "Rife",
   "Soliloquy",
@@ -28,6 +32,26 @@ terms <- c(
   "Hubris",
   "Peristeronic",
   "Salient")
+
+
+f.gtrend.csv.read <- function(x){
+  read.csv(file=paste0("data/report_", sprintf("%02d", as.numeric(x)), ".csv"), 
+           na.strings = " ", 
+           stringsAsFactors = FALSE,
+           # 173 is last timeseries row, minus skip, minus header = 168
+           skip = 4, nrows = 168)
+}
+
+# merge all four google .csvs 
+data.raw <- f.gtrend.csv.read(1) %>% 
+  merge(f.gtrend.csv.read(2)) %>% 
+  merge(f.gtrend.csv.read(3)) %>% 
+  merge(f.gtrend.csv.read(4)) %>% 
+  mutate(
+    Time = as.POSIXct(strptime(Time, "%Y-%m-%d-%H:%M UTC", tz="UTC"))
+  )
+
+
 
 
 
