de05b04c0979f23fb79a04f93ec653881faa0c92
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-09-03 16:39:15 +0200

    first GTrendsR tries/fails...

--- xkcd_words_impact.R
+++ xkcd_words_impact.R
@@ -31,4 +31,27 @@ terms <- c(
 
 
 
+# https://trinkerrstuff.wordpress.com/2014/11/24/gtrendsr-package-to-explore-google-trending-for-field-dependent-terms/
+# devtools::install_github("dvanclev/GTrendsR")
+# devtools::install_github("trinker/gtrend")
+library(GTrendsR)
+library(gtrend)
+
+phrases <- sprintf("\"%s\"", gsub("[-,]", " ", terms))
+
+
+google.raw <- gtrend_scraper("christoph.safferling@googlemail.com", "password", terms)
+#http://www.google.com/trends/explore#q=Soliloquy%2C%20Cadine&date=now%207-d&cmpt=q&tz=Etc%2FGMT-2
+
+usr <- "christoph.safferling@googlemail.com"
+psw <- "password"
+ch <- GTrendsR::gconnect(usr, psw)  # or use env.var or options 
+
+location <- "CA"
+query <- "NHL"
+
+data = gtrends(ch, query = query, geo = location)
+
+ch <- GTrendsR::gconnect(usr, psw)
+
 
