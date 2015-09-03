8154762bbb92f9a9832f815d0a3f278caf41c308
Christoph Safferling <christoph.safferling@bluebyte.de>
2015-09-03 16:38:35 +0200

    inital setup, save xkcd survey words

---
+++ xkcd_words_impact.R
@@ -0,0 +1,34 @@
+options(bitmapType='cairo')
+options(scipen = 999)
+# library(bbbi)
+# library(data.table)
+library(dplyr)
+# library(tidyr)
+library(ggplot2)
+
+terms <- c(
+  "NHL", 
+  "Unitory", 
+  "Rife",
+  "Soliloquy",
+  "Regolith",
+  "Tribution",
+  "Stipple",
+  "Fination",
+  "Modicum",
+  "Trephony",
+  "Amiable",
+  "Fleek",
+  "Apricity",
+  "Lithe",
+  "Revergent",
+  "Cadine",
+  "Phoropter",
+  "Slickle",
+  "Hubris",
+  "Peristeronic",
+  "Salient")
+
+
+
+
